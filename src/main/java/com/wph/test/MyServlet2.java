73b5f794f9b30f1aac3aa6e27a1a94c33b88bcd3
wph1 <1217511705@qq.com>
2017-12-06 13:48:11 +0800

    自己手动创建的第二个servlet

---
+++ src/main/java/com/wph/test/MyServlet2.java
@@ -0,0 +1,22 @@
+package com.wph.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2017/12/6.
+ */
+@WebServlet(name = "MyServlet2")
+public class MyServlet2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("自己手动添加的git");
+    }
+}
